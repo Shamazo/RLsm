d22eb036f0c5621c86bc4a53bf0ffb28ea64737e
shamazo <hmnicholson12@gmail.com>
2020-12-22 19:24:53 -0500

    add flatbuffers as a submodule inorder to get latest rust support
    Also implemented a script to generate flatbuffers for the python and rust clients.

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # RLsm
-A LSM based key-value store written in rust
+A (WIP) LSM based key-value store written in rust
+
+### Flatbuffers
+To have decent Rust support with flatbuffers we need to use the latest master. To make things easier this repo has a flatbuffers submodule.  To get started clone this repo with the `--recurse-submodules` option. Then run `build_flatbuffers.py` in the root of the repo to generate flatbuffer code for the python and rust clients. 
 
 to do 
-- rust build script to compile flat buffers for python and for rust
-- github actions to build and run tests 
+- github actions to build and run tests
+- design/implement benchmarks 
+- design doc 
---
+++ build_flatbuffers.py
@@ -0,0 +1,23 @@
+import os
+
+print("configuring flatbuffers cmake")
+os.chdir("flatbuffers")
+cmake = os.popen('cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Release')
+output = cmake.read()
+print(output)
+
+print("Making flatbuffers")
+make = os.popen("make all")
+output = make.read()
+# print(output)
+
+print("Generating flatbuffers for python")
+python_buffers = os.popen("./flatc --python -o ../systemtesting/ ../libmessages/flatbuffers/api.fbs --gen-all")
+
+output = python_buffers.read()
+# print(output)
+
+print("generating flatbuffers for rust")
+rust_buffers = os.popen("./flatc --rust -o ../libmessages/src/ ../libmessages/flatbuffers/api.fbs --gen-all")
+output = rust_buffers.read()
+# print(output)
